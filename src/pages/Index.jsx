0de50ff2d7ceac1584f141dd9d9631112d2841c7
iamvipora <henrydizon99@gmail.com>
2024-07-16 18:41:22 +0800

    adjusted buttons width

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -7,9 +7,9 @@ function Index({ background }) {
 
   return (
     <div className='h-screen w-screen flex text-white bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url(${background})` }}>
-      <div className='h-full w-full flex flex-col p-8 font-press-start text-center justify-between'>
+      <div className='w-full flex flex-col p-8 font-press-start text-center justify-between'>
         <h1 className='text-3xl'>Arcane Duel</h1>
-        <div className='flex flex-col text-xl gap-4 justify-center'>
+        <div className='w-full flex flex-col items-center text-xl gap-4'>
           <Link to='/combat'>
             <h1 className='hover:text-[#D7D7D7]'>Play</h1>
           </Link>
@@ -17,7 +17,7 @@ function Index({ background }) {
             <h1 className='hover:text-[#D7D7D7]'>Inventory</h1>
           </Link>
           <Link to='/shop'>
-              <h1 className='hover:text-[#D7D7D7]'>Shop</h1>
+            <h1 className='hover:text-[#D7D7D7]'>Shop</h1>
           </Link>
         </div>
         <footer className='flex items-center place-content-center gap-4 text-3xl  '>
